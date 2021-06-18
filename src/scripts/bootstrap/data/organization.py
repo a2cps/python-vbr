ef29afded15626a72a997f20b1f5162ebfae9cb1
vaughn <vaughn@tacc.utexas.edu>
2021-06-18 11:01:15 -0500

    Base data for organization is now CSV based

--- src/scripts/bootstrap/data/organization.py
+++ src/scripts/bootstrap/data/organization.py
@@ -2,39 +2,4 @@ from .loader import TableData
 
 
 class OrganizationData(TableData):
-    DATA = [{
-        'organization_id': 1,
-        'url': 'tacc.utexas.edu',
-        'name': 'TACC',
-        'description': 'Data Coordinating Center'
-    }, {
-        'organization_id': 2,
-        'url': 'uic.edu',
-        'name': 'UIC',
-        'description': 'University of Illinois at Chicago'
-    }, {
-        'organization_id': 3,
-        'url': 'rush.edu',
-        'name': 'Rush',
-        'description': 'Rush University Medical Center'
-    }, {
-        'organization_id': 4,
-        'url': 'chicagoitm.org',
-        'name': 'ITM',
-        'description': 'Institute for Translational Medicine'
-    }, {
-        'organization_id': 5,
-        'url': 'uchicagomedicine.org',
-        'name': 'UCMC',
-        'description': 'University of Chicago Medical Center'
-    }, {
-        'organization_id': 6,
-        'url': 'northshore.org',
-        'name': 'NS',
-        'description': 'NorthShore'
-    }, {
-        'organization_id': 7,
-        'url': 'umich.edu',
-        'name': 'UMICH',
-        'description': 'University of Michigan'
-    }]
+    pass
