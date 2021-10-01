0960c4b45372b47848807460c4d96f88c295f19b
vaughn <vaughn@tacc.utexas.edu>
2021-10-01 12:52:11 -0500

    Updated vbr.pgrest.foreign_key to support on_event and event_action available in pgrest 1.0.2

--- src/vbr/pgrest/foreign_key.py
+++ src/vbr/pgrest/foreign_key.py
@@ -2,26 +2,30 @@ from .config import Config
 
 __all__ = ['ForeignKey']
 
+ON_EVENT_ALLOWED = ['ON DELETE', 'ON UPDATE']
+EVENT_ACTION_ALLOWED = [
+    'CASCADE', 'SET_NULL', 'SET DEFAULT', 'RESTRICT', 'NO ACTION'
+]
+
 
 class ForeignKey(object):
-    def __init__(self, source, on_delete='cascade', on_update='cascade'):
+    def __init__(self, source, on_event='ON UPDATE', event_action='CASCADE'):
         self.foreign_key = True
         (self.reference_table, self.reference_column) = source.split('.')
-        self.on_delete = on_delete
-        self.on_update = on_update
+
+        self.on_event = on_event.upper()
+        if self.on_event not in ON_EVENT_ALLOWED:
+            raise ValueError('Invalid value for on_event')
+        self.event_action = event_action.upper()
+        if self.event_action not in EVENT_ACTION_ALLOWED:
+            raise ValueError('Invalid value for event_action')
 
     def properties(self):
 
-        # Feature gate: Use more descriptive name 'foreign_key'
-        # https://github.com/tapis-project/paas/issues/6
-        if Config.COLUMN_USE_FOREIGN_KEY_PROPERTY_NAME:
-            fk_prop_name = 'foreign_key'
-        else:
-            fk_prop_name = 'FK'
-
         return {
-            fk_prop_name: self.foreign_key,
+            'foreign_key': self.foreign_key,
             'reference_table': self.reference_table,
             'reference_column': self.reference_column,
-            'on_delete': self.on_delete
+            'on_event': self.on_event,
+            'event_action': self.event_action
         }
