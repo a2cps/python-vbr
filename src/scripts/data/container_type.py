3649ceb4bd0c401cf7849672234d2339ce9c1209
vaughn <vaughn@tacc.utexas.edu>
2021-10-13 12:50:13 -0500

    Update container types, protocols, reasons to model sample handling workflow and outcomes

--- src/scripts/data/container_type.py
+++ src/scripts/data/container_type.py
@@ -18,5 +18,21 @@ class ContainerTypeData(TableData):
         {
             'container_type_id': 4,
             'name': 'paxgene freezer box'
+        },
+        {
+            'container_type_id': 5,
+            'name': '-80C freezer'
+        },
+        {
+            'container_type_id': 6,
+            'name': '-20C freezer'
+        },
+        {
+            'container_type_id': 7,
+            'name': '4C refrigerator'
+        },
+        {
+            'container_type_id': 8,
+            'name': 'liquid nitrogen storage'
         }
     ]
