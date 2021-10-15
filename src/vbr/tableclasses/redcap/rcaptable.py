40176ff359b7d424d674f7cf5f68ef5c897a20dd
vaughn <vaughn@tacc.utexas.edu>
2021-10-15 10:45:35 -0500

    Refactored RcapTable to be more programatically usable by the RcapTableApi class that is being developed.

--- src/vbr/tableclasses/redcap/rcaptable.py
+++ src/vbr/tableclasses/redcap/rcaptable.py
@@ -3,29 +3,7 @@ from ..constants import Constants
 from .rcconstants import REDCapConstants
 
 
-class RcapSubjectAssociation(object):
-    """Associate a REDcap table entry to a VBR.subject"""
-    subject_id = Column(Integer,
-                        ForeignKey("subject.subject_id"),
-                        nullable=True)
-
-
-class RcapBiosampleAssociation(object):
-    """Associate a REDcap table entry to a VBR.biosample"""
-    sample_id = Column(Integer,
-                       ForeignKey("biosample.biosample_id"),
-                       nullable=True)
-
-
-class RcapDataEventAssociation(object):
-    """Associate a REDcap table entry to a VBR.data_event"""
-    data_event_id = Column(Integer,
-                           ForeignKey("data_event.data_event_id"),
-                           nullable=True)
-
-
-class RcapTable(RcapSubjectAssociation, RcapBiosampleAssociation,
-                RcapDataEventAssociation, Table):
+class RcapTable(Table):
     """Parent class for REDCap-derived tables
     """
     # persistent_id (subject GUID)
@@ -36,3 +14,25 @@ class RcapTable(RcapSubjectAssociation, RcapBiosampleAssociation,
     # RCap record_id identifying subject
     record_id = REDCapConstants.RECORD_ID
     # TODO - Do we need a uniqueness constraint on the set of id fields?
+    data_event_id = Column(Integer,
+                           ForeignKey("data_event.data_event_id"),
+                           nullable=True)
+    biosample_id = Column(Integer,
+                    ForeignKey("biosample.biosample_id"),
+                    nullable=True)
+    measurement_id = Column(Integer,
+                    ForeignKey("measurement.measurement_id"),
+                    nullable=True)
+    subject_id = Column(Integer,
+                        ForeignKey("subject.subject_id"),
+                        nullable=True)
+    @classmethod
+    def _links(cls):
+        # Update this with any new linkages established by adding FK constraints above
+        return ('data_event', 'biosample', 'measurement', 'subject')
+
+    @classmethod
+    def link_column_names(cls):
+        """Return names of columns linked by foreign key constraints."""
+        cols = [c + '_id' for c in cls._links()]
+        return tuple(cols)
