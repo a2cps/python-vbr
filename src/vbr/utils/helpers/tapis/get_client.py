1096d5b242bd79d7e2ea0e135b9e0d7f633b988a
vaughn <vaughn@tacc.utexas.edu>
2021-11-01 11:17:26 -0500

    Avoid infinite recursion error with get_client

--- src/vbr/utils/helpers/tapis/get_client.py
+++ src/vbr/utils/helpers/tapis/get_client.py
@@ -1,5 +1,5 @@
 import tapipy
-from tapipy.actors import get_client
+from tapipy.actors import actors
 from .tapis_local_cache import TapisLocalCache
 
 __all__ = ['get_client']
@@ -8,7 +8,7 @@ __all__ = ['get_client']
 def get_client():
     try:
         # Works inside a Tapis Actor.
-        return get_client()
+        return actors.get_client()
     except tapipy.errors.BaseTapyException:
         # Works on configured local host
         return TapisLocalCache.restore()
