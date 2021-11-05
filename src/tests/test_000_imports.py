dfab7dc92694705d40803f58372acbc4192af04e
vaughn <vaughn@tacc.utexas.edu>
2021-11-05 05:26:37 -0500

    Added import tests

---
+++ src/tests/test_000_imports.py
@@ -0,0 +1,24 @@
+def test_package_import():
+    try:
+        import vbr
+    except ImportError:
+        raise
+
+def test_pgrest_submodule_imports():
+    try:
+        from vbr import pgrest
+    except ImportError:
+        raise
+
+def test_client_submodule_imports():
+    try:
+        from vbr.client import VBR
+    except ImportError:
+        raise
+
+
+def test_api_submodule_imports():
+    try:
+        from vbr.api import get_vbr_api_client, VBR_Api
+    except ImportError:
+        raise
