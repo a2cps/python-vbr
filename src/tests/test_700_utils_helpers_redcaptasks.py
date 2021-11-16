4ce126ae0a4bccd0292856d6d3a69a77cd44b5fe
vaughn <vaughn@tacc.utexas.edu>
2021-11-16 10:01:09 -0600

    Refactor redcap_to_pgrest_datetime into redcap_to_pgrest_datetime and redcap_to_datetime

--- src/tests/test_700_utils_helpers_redcaptasks.py
+++ src/tests/test_700_utils_helpers_redcaptasks.py
@@ -31,3 +31,21 @@ def test_redcap_event_id_to_unique_event_name_valuerror():
 @pytest.mark.parametrize("test_input,expected", [("14", "2"), ("29", "3"), ("25", "1")])
 def test_redcap_project_id_to_vbr_project(test_input, expected):
     assert vbr.utils.redcaptasks.redcap_to_vbr_project_id(test_input) == expected
+
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        ("2001-04-12 19:03", "2001-04-13T01:03:00.000000Z"),
+        ("2001-04-12 19:03:24", "2001-04-13T01:03:24.000000Z"),
+        ("2001-04-12 19:03:24.000001", "2001-04-13T01:03:24.000001Z"),
+        ("2001-04-12", None),
+        ("2001-04-12T19:03:24", "2001-04-13T01:03:24.000000Z"),
+    ],
+)
+def test_redcap_to_pgrest_datetime(test_input, expected):
+    if expected is None:
+        with pytest.raises(ValueError):
+            vbr.utils.redcaptasks.redcap_to_pgrest_datetime(test_input)
+    else:
+        assert vbr.utils.redcaptasks.redcap_to_pgrest_datetime(test_input) == expected
--- src/vbr/utils/redcaptasks/time.py
+++ src/vbr/utils/redcaptasks/time.py
@@ -5,19 +5,22 @@ import pytz
 
 from .constants import REDCAP_SERVER_TIMEZONE
 
-__all__ = ["redcap_to_pgrest_datetime"]
+__all__ = ["redcap_to_pgrest_datetime", "redcap_to_datetime"]
 
 
-def redcap_to_pgrest_datetime(date_time_str: str, source_timezone: str = None) -> str:
-    """Tranform REDcap time strings into ISO-8601 format."""
+def redcap_to_datetime(date_time_str: str, source_timezone: str = None) -> str:
+    """Tranform REDcap time strings into datetime"""
     if source_timezone is None:
-        source_timezone = os.environ.get(REDCAP_SERVER_TIMEZONE, "America/Chicago")
+        source_timezone = "America/Chicago"
 
     # 2021-04-30 11:53 -> 2021-04-30T11:53:00.00000
     # Source formats
-    SOURCES = ("%Y-%m-%d %H:%M", "%Y-%m-%d %H:%M:%S", "%Y-%m-%d %H:%M:%S.%f")
-    # Destination format
-    DEST = "%Y-%m-%dT%H:%M:%S.%fZ"
+    SOURCES = (
+        "%Y-%m-%d %H:%M",
+        "%Y-%m-%d %H:%M:%S",
+        "%Y-%m-%d %H:%M:%S.%f",
+        "%Y-%m-%dT%H:%M:%S",
+    )
     timezone_now = pytz.timezone(source_timezone)
     # UTC
     timezone_utc = pytz.timezone("Europe/London")
@@ -26,8 +29,15 @@ def redcap_to_pgrest_datetime(date_time_str: str, source_timezone: str = None) -
             dt = datetime.datetime.strptime(date_time_str, f)
             dtl = timezone_now.localize(dt)
             dtutc = dtl.astimezone(timezone_utc)
-            return dtutc.strftime(DEST)
+            return dtutc
         except ValueError:
-            raise ValueError(
-                "{0} not in a recognized strptime format".format(date_time_str)
-            )
+            pass
+    raise ValueError("{0} not in a recognized strptime format".format(date_time_str))
+
+
+def redcap_to_pgrest_datetime(date_time_str: str, source_timezone: str = None) -> str:
+    """Tranform REDcap time strings into ISO-8601 format."""
+    # Destination format
+    DEST = "%Y-%m-%dT%H:%M:%S.%fZ"
+    dtutc = redcap_to_datetime(date_time_str, source_timezone)
+    return dtutc.strftime(DEST)
