c22ffd2ee0aeddd0efc169203992285916d50b95
vaughn <vaughn@tacc.utexas.edu>
2021-11-29 09:39:07 -0600

    Organization by name

--- src/vbr/api/organization.py
+++ src/vbr/api/organization.py
@@ -4,10 +4,11 @@ __all__ = ["OrganizationApi"]
 
 
 class OrganizationApi(object):
-    def get_location(self, pkid: str) -> Organization:
+    def get_organization(self, pkid: str) -> Organization:
         """Retrieve a Organization by primary identifier."""
         return self._get_row_from_table_with_id("organization", pkid)
 
-    def get_location_by_local_id(self, local_id: str) -> Organization:
-        """Retrieve a Organization by local_id."""
-        return self._get_row_from_table_with_local_id("organization", local_id)
+    def get_organization_by_name(self, name: str) -> Organization:
+        """Retrieve a Organization by name."""
+        query = {"name": {"operator": "=", "value": name}}
+        return self._get_row_from_table_with_query("organization", query)
