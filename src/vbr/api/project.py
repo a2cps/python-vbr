3b728ac3de7a04d94040a5c20549f2d600a33db6
vaughn <vaughn@tacc.utexas.edu>
2021-12-14 14:12:35 -0600

    added get_project_by_local_id

--- src/vbr/api/project.py
+++ src/vbr/api/project.py
@@ -9,6 +9,10 @@ class ProjectApi(object):
         """Retrieve a Project by primary identifier."""
         return self._get_row_from_table_with_id("project", pkid)
 
+    def get_project_by_local_id(self, local_id: str) -> Project:
+        """Retrieve a Project by local_id."""
+        return self._get_row_from_table_with_local_id("project", local_id)
+
     def redcap_to_vbr_project_id(self, redcap_project_id: str) -> str:
         """Return the VBR project_id for a given REDCap project identifier."""
         vbr_project_id = redcaptasks.redcap_to_vbr_project_id(redcap_project_id)
