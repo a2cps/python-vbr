17dc636bec78438e5c4fb28410718cbf22a83a1c
vaughn <vaughn@tacc.utexas.edu>
2022-04-22 16:06:49 -0500

    Added create_collection_type and create_container_type APIs

--- src/vbr/api/collection_type.py
+++ src/vbr/api/collection_type.py
@@ -11,3 +11,14 @@ class CollectionTypeApi(object):
     def get_collection_type_by_local_id(self, local_id: str) -> CollectionType:
         """Retrieve a CollectionType by local_id."""
         return self._get_row_from_table_with_local_id("collection_type", local_id)
+
+    def create_collection_type(
+        self, name: str, description: str = None
+    ) -> CollectionType:
+        """Create a new CollectionType."""
+        # TODO - Add a data_event marking the creation
+        ct = CollectionType(name=name, description=description)
+        try:
+            return self.vbr_client.create_row(ct)[0]
+        except Exception:
+            raise
--- src/vbr/api/container_type.py
+++ src/vbr/api/container_type.py
@@ -11,3 +11,14 @@ class ContainerTypeApi(object):
     def get_container_type_by_local_id(self, local_id: str) -> ContainerType:
         """Retrieve a ContainerType by local_id."""
         return self._get_row_from_table_with_local_id("container_type", local_id)
+
+    def create_container_type(
+        self, name: str, description: str = None
+    ) -> ContainerType:
+        """Create a new ContainerType."""
+        # TODO - Add a data_event marking the creation
+        ct = ContainerType(name=name, description=description)
+        try:
+            return self.vbr_client.create_row(ct)[0]
+        except Exception:
+            raise
