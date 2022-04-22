23045aa770ded207d94c48fe394bb9e9e639de3f
vaughn <vaughn@tacc.utexas.edu>
2022-04-22 14:21:00 -0500

    Added exRNA and other types of run list

--- src/scripts/data/collection_type.py
+++ src/scripts/data/collection_type.py
@@ -4,5 +4,10 @@ from .loader import TableData
 class CollectionTypeData(TableData):
     DATA = [
         {"collection_type_id": 0, "name": "generic collection"},
-        {"collection_type_id": 1, "name": "run list"},
+        {"collection_type_id": 1, "name": "run list:generic"},
+        {"collection_type_id": 2, "name": "run list:exRNA"},
+        {"collection_type_id": 3, "name": "run list:genetic variants"},
+        {"collection_type_id": 4, "name": "run list:proteomics"},
+        {"collection_type_id": 5, "name": "run list:lipodomics"},
+        {"collection_type_id": 6, "name": "run list:metabolomics"},
     ]
