a5f9190d425217905e5a9c425d14d06f4f54f5cf
Pat Scherer <72883517+pscherer-tacc@users.noreply.github.com>
2022-08-08 13:12:02 -0500

    Update redcap_classfiles.py
    REDCap fieldtypes and common variables added to autogenerated table schemas.

--- src/scripts/redcap_classfiles.py
+++ src/scripts/redcap_classfiles.py
@@ -1,22 +1,46 @@
 def choices_to_dict(choices_str: str) -> dict:
-    # REDcap choices are expressed as pipe-delimited sets of comma-delim values
-    #
-    # Used to populate dropdown, checkbox, or slider types
-    #
-    # Example: 1, Yes | 0, No
-    # Example: 0, 0 | .25, 0.25 | .5, 0.5 | 1, 1 | 2, 2 | 4, 4 | 8, 8
-    # Example: 1, Unable to obtain blood sample -technical reason | 2, Unable to obtain blood sample -patient related | 3, Sample handling/processing error
-    cdict = {}
-    for i in choices_str.split("|"):
-        i = i.strip()
-        ii = i.split(",")
-        if len(ii) > 1:
-            k = ii[0].strip()
-            v = ii[1].strip()
-            cdict[k] = v
-    return cdict
+# Utility adapted/extended from code written by M. Vaughn at TACC to parse a REDCap metadata dictionary to generate table schemas and python framework within a database 
+# for storing REDCap instrument data. The original code supported a subset of REDCap "field types" and did not utilize other fields present in the REDCap data definitions to
+# to specify datatypes (such as integer, numeric, datetime formats), required fields, or valid range. Extensions to this code are intended to leverage this information
+# when present in the data dictionary. "Required" and text validation ranges are not enforced by REDCap so will be captured as information but not used to define the table schema.
+# This code also supports creating a single set of table schemas from a merged set of REDCap projects with variables added to filter
+# tables by project, organization, and other useful criteria. This code is currently under development. 
+#
+# REDCap data are all exported as strings that require parsing and datatype reclassification to be useful. REDCap "field_types" are as follows: 
+#   "text" and "notes" are stored as strings
+#   "descriptive" does not directly associate to a data entry and can generally be ignored
+#   "yesno" and "truefalse" are boolean, but may need to be typed as string if data is not consistent
+#   "radio" is a single answer formatted as an integer (raw data ennumeration), comma separator, and label. Raw data exports will only contain the integer associated with the answer.  
+#   "dropdown" is a single answer formatted as an integer (raw data ennumeration), comma separator, and label. Raw data exports will only contain the integer associated with the answer.
+#   "checkbox" can be multiple answers expressed as pipe-delimited sets of comma-delimited values. REDCap data can be exported/stored in this format 
+#       or as separate variables containing a triple underscore and enumeration (for example, "___1"). Storing multiselect answers as separate variables has generally
+#       been the preferred approach for supporting data analysis.
+#   "calc" is a formula used to auto-calculate answers based on other fields within the REDCap project. The exported answers could be almost any datatype based on the logic. 
+#   "slider" provides a single answer (integer) on a scale of a defined range and slider position. A common range used is 0-100.
+#   "file" can be stored as a string or URL
+#
+# The original code only handles a single checkbox selection. If field_type = "checkbox" AND choices contain a pipe separator indicating multiple checkboxes are present,
+# create a separate variable for each choice as "[field_name]___[choice integer]". An example of multi-select checkboxes is early_withdrawal_v04.ewdisreasons.
+#
+# Used to populate dropdown, checkbox, or slider type
+# Generated classes are camelcase; tables are snakecase.
+#
+# Example: 1, Yes | 0, No
+# Example: 0, 0 | .25, 0.25 | .5, 0.5 | 1, 1 | 2, 2 | 4, 4 | 8, 8
+# Example: 1, Clinic | 2, Hospital | 3, Specialist
+#
+# The FOR loop below splits each string containing raw and labeled data into separate answers and strips the pipe and comma separators. 
+cdict = {}
+for i in choices_str.split("|"):
+    i = i.strip()
+    ii = i.split(",")
+    if len(ii) > 1:
+        k = ii[0].strip()
+        v = ii[1].strip()
+        cdict[k] = v
+return cdict
 
-
+# Testing choices to determine if it is an integer is more straightforward than checking for "integer" in text_validation.
 def choices_are_integer(choices_str: str) -> bool:
     cdict = choices_to_dict(choices_str)
     for k, _ in cdict.items():
@@ -26,7 +50,7 @@ def choices_are_integer(choices_str: str) -> bool:
             return False
     return True
 
-
+# Testing choices to determine if it is a numeric is more straightforward than checking for "number" in text_validation.
 def choices_are_numeric(choices_str: str) -> bool:
     cdict = choices_to_dict(choices_str)
     for k, _ in cdict.items():
@@ -36,7 +60,7 @@ def choices_are_numeric(choices_str: str) -> bool:
             return False
     return True
 
-
+# This case should cover truefalse field_type
 def choices_are_boolean(choices_str: str) -> bool:
     cdict = choices_to_dict(choices_str)
     values = []
@@ -53,7 +77,7 @@ def choices_are_boolean(choices_str: str) -> bool:
             return False
     return True
 
-
+# Have we tested this? Both yesno and truefalse REDCap field types are supposed to rendor "1" or "0" as answers.
 def choices_are_yesno(choices_str: str) -> bool:
     cdict = choices_to_dict(choices_str)
     values = []
@@ -66,6 +90,18 @@ def choices_are_yesno(choices_str: str) -> bool:
             return False
     return True
 
+# Added truefalse field handling just in case it is NOT handled by the boolean case above.
+def choices_are_truefalse(choices_str: str) -> bool:
+    cdict = choices_to_dict(choices_str)
+    values = []
+    for k, _ in cdict.items():
+        values.append(k)
+    if len(values) != 2:
+        return False
+    for v in values:
+        if v not in ["1", "0"]:
+            return False
+    return True
 
 def process_data_dict(filename: str, current: dict = None) -> dict:
     """Convert a REDcap Data Dictionary into a minimally processed, actionable Python dict"""
@@ -75,6 +111,9 @@ def process_data_dict(filename: str, current: dict = None) -> dict:
     else:
         ddict = current
 
+# Expand the code below to capture: text_validation, text_validation_min, and text_vaidation_max. These entries, when used, 
+# can translate directly into datatypes and ranges for the data schema. "Required" is not generally enforced so less useful for schema definition.  
+        
     with open(filename, "r") as csvfile:
         ddreader = csv.reader(csvfile)
         # Strip header
@@ -90,13 +129,19 @@ def process_data_dict(filename: str, current: dict = None) -> dict:
             field_label = row[4]
             # Choices
             choices = row[5]
+            # Text Validation (integer, number, date_mdy, date_ymd, date_dmy, datetime_mdy, datetime_ymd, datetime_dmy, datetime_seconds_mdy, datetime_seconds_ymd, datetime_seconds_dmy, time, email, phone, signature)
+            text_validation = row[7]
+            # Text Validation Min
+            text_validation_min = row[8]
+            # Text Validation Max
+            text_validation_max = row[9]
             # Identifier
             identifier = row[10]
             if identifier == "y":
                 identifier = True
             else:
                 identifier = False
-
+                           
             if form_name != "" and var_field_name != "":
                 if (
                     field_type in SUPPORTED_STRING_TYPES
@@ -162,8 +207,7 @@ def build(args):
             # Translation rules #
             #####################
 
-            # Columns are always nullable (at least
-            # until we decide what columns are mandatory)
+            # Columns are nullable (until required data is enforced via the REDCap data source)
             col_nullable = "True"
 
             # Empty default comment
@@ -174,11 +218,13 @@ def build(args):
                 col_type = "GUID"
             if col_config.get("field_type", None) == "yesno":
                 col_type = "Boolean"
+            if col_config.get("field_type", None) == "truefalse":
+                col_type = "Boolean"
+
 
             # Process 'Choices' field
             col_choices = col_config.get("choices", "")
-            # Try to override type to be more descriptive of field value
-            # based on choices available in REDcap form
+            # Try to override type to be more descriptive of field value based on choices available in REDcap form
             if col_choices != "":
                 # TODO Implement casting/handling of string, numeric values to 1/0
                 if choices_are_boolean(col_choices) or choices_are_yesno(col_choices):
@@ -236,6 +282,10 @@ def build(args):
                 "type": col_type,
             }
 
+# The following REDCap common fields should be added to each REDCap instrument table within the VBR: persistent_id (subject guid), project_id (REDCap project_id "pid"), 
+# organization_id (REDCap "dag"), protocol_id (maps to REDCap event_name), creation_time, last_updated_ts, language 
+# These were once included in class RcapTable but may have been refactored or omitted. Should these common variables now be added via the Jinja template or somewhere else? 
+            
         # Render and write the class file from Jinja template
         with open(os.path.join(TEMPLATES_DIR, CLASS_TEMPLATE)) as tf:
             template = Template(tf.read())
@@ -318,7 +368,8 @@ if __name__ == "__main__":
 
     CLASS_TEMPLATE = "redcap_tableclass.py.j2"
     INIT_TEMPLATE = "redcap_tableclasses_init.py.j2"
-    SUPPORTED_STRING_TYPES = ["text", "radio", "dropdown", "checkbox", "yesno"]
+    # SUPPORTED_STRING_TYPES = ["text", "radio", "dropdown", "checkbox", "yesno"]
+    SUPPORTED_STRING_TYPES = ["text", "radio", "dropdown", "checkbox", "yesno", "truefalse", "slider", "file"]
     SUPPORTED_FREETEXT_TYPES = ["notes"]
     parser = get_parser()
     parser.add_argument(
