c817ffcb8207551222a354849fb0864094d6fc57
urrutia <jurrutia@tacc.utexas.edu>
2022-12-15 12:14:06 -0600

    adding version to vbr class

--- src/vbr/__init__.py
+++ src/vbr/__init__.py
@@ -3,6 +3,7 @@
 from . import api, pgrest, tableclasses, utils
 import importlib.metadata
 
+# imports from setup.cfg
 __version__ = importlib.metadata.version('python-vbr')
 
 # from .client import VBR
