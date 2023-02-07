7b3a8c36618bf9efeb94c000bdf3699d4941767f
urrutia <jurrutia@tacc.utexas.edu>
2023-02-07 13:52:49 -0600

    quick patch for repeat instance/instrument to blood draw table

--- src/vbr/tableclasses/redcap/autogenerated/blood_sample_collection_and_processing_crf.py
+++ src/vbr/tableclasses/redcap/autogenerated/blood_sample_collection_and_processing_crf.py
@@ -150,3 +150,8 @@ class RcapBloodSampleCollectionAndProcessingCrf(RcapTable):
     # Field Type: text
     # Choices: N/A
     bscp_procby_initials = Column(String, nullable=True, comments=None)
+    # Add this to the autogen script instead
+    redcap_repeat_instance = Column(String, nullable=True, comments=None)
+    redcap_repeat_instrument = Column(String, nullable=True, comments=None)
+    
+    
